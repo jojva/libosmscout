0ed3122d84e3d0475ae4f57a508f3d49d58fa2aa
Joris Valette <joris.valette@cofelyineo-gdfsuez.com>
2016-01-28 15:39:09 +0100

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo rm -rf /usr/local/include/osmscout
+sudo cp -r libosmscout/include/osmscout /usr/local/include/
+sudo cp -r libosmscout-import/include/osmscout /usr/local/include/
+sudo cp -r libosmscout-map/include/osmscout /usr/local/include/
+sudo cp -r libosmscout-map-cairo/include/osmscout /usr/local/include/
